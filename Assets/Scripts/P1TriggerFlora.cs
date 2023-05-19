16bc89bce7b2cb15267773fb36724af8e197d7a0
GMMushroom <69878376+GMMushroom@users.noreply.github.com>
2023-05-19 10:02:45 +1000

    Health bars in place.
    Put health bars on canvas. That's it.

---
+++ Assets/Scripts/P1TriggerFlora.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P1TriggerFlora : MonoBehaviour
+{
+    public Collider2D Col;
+    private Animator Anim;
+    private AnimatorStateInfo Player1Layer0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Anim = GetComponentInParent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Listens to Animator
+        Player1Layer0 = Anim.GetCurrentAnimatorStateInfo(0);
+
+        //Enabling Colliders
+        if (P1ActionFlora.Hits == false)
+        {
+            Col.enabled = true;
+            Anim.SetTrigger("Connects");
+        }
+        else
+        {
+            Col.enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player2"))
+        {
+            P1ActionFlora.Hits = true;
+        }
+    }
+}
